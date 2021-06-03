c502f8cedf92e8e7f34a81a6b153d367e9a06511
cc <cc@ccs-iMac.local>
2021-06-03 15:06:31 +0200

    new features

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,20 @@
 
 /************************************************************************
 
-  Restore_Kernel_Switch_W27E257
+  Restore_Kernal_Switch_W27E257
 
-    "64 Tuning Board" - Kernel Switch, ATTiny84A Firmware, Internal 8Mhz (hfuse 0xdf | lfuse 0xe2)
+    "64 Tuning Board" - Kernal Switch, ATTiny84A firmware, internal clock 8Mhz (hfuse 0xdf | lfuse 0xe2)
 
     Designed by Matthias Lorenz
+
+    changes 2021 by cassy
+
+    - power on reset added, changed PIN defnitions for PlatformIO, typos 
+    - a manual reset WITHOUT switching Kernal can be performed by pressing the Restore key for 800 - 2000 ms
+    - true WHITE LED multiplexing added during RESET procedure
+
+    burning fuses:  minipro -p attiny84 -w fuses.bin -c config
+    flashing fw:    minipro -p attiny84 -w firmware.hex
 
  ************************************************************************
 
@@ -21,13 +30,13 @@
 Warning !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 Pin definition is COUNTER clock wise when using PlatformIO
 See pin definition on the "inside" of the ic pinout below 
-
-              ATTINY84A-PU
-              ------------
-
+                             |
+              ATTINY84A-PU   |
+              ------------   |
+                            / \
                     +5V o    ^    o GND
-               XTAL/D10 o 0    10 o D0/A0  -> OUT KERNEL "PIN A15" (J2)     Vpp = 27E257 = always HIGH
-               XTAL/D9  o 1     9 o D1/A1  -> OUT KERNEL "PIN A14" (J15)    A14 = 27E257
+               XTAL/D10 o 0    10 o D0/A0  -> OUT KERNAL "PIN A15" (J2)     Vpp = 27E257 = always HIGH
+               XTAL/D9  o 1     9 o D1/A1  -> OUT KERNAL "PIN A14" (J15)    A14 = 27E257
             (ICSP) RES  o 11    8 o D2/A2  <- IN "RESTORE KEY"
   (ROT)             D8  o 2     7 o D3/A3  -> _RESET Pin C64
   (GRÜN)         A7/D7  o 3     6 o D4/A4  SCK
@@ -37,8 +46,8 @@ See pin definition on the "inside" of the ic pinout below
 
 #include <EEPROM.h>
 
-int kernePIN14 = 9;   // was 1
-int kernePIN15 = 10;   // was 0   
+int kernalPIN14 = 9;   // was 1
+int kernalPIN15 = 10;   // was 0   
 int restoreKey = 8;   // was 2
 int resetPIN   = 7;   // was 3
 
@@ -63,7 +72,7 @@ void red() {
 
 void green() {
   analogWrite(ledR, 0);
-  analogWrite(ledG, 155);
+  analogWrite(ledG, 255);
   analogWrite(ledB, 0);
 }
 
@@ -79,18 +88,17 @@ void violet() {
   analogWrite(ledB, 180);
 }
 
-
 void doState() {
 
   if (state == 0) {
     blue();
-    digitalWrite(kernePIN14, LOW);    // Kernel: LOW Address
-    digitalWrite(kernePIN15, HIGH);   // Vpp must be STAY HIGH
+    digitalWrite(kernalPIN14, LOW);    // Kernal: LOW Address
+    digitalWrite(kernalPIN15, HIGH);   // Vpp must be STAY HIGH
   }
   if (state == 1) {
     violet();
-    digitalWrite(kernePIN14, HIGH);  // Kernel: HIGH Address
-    digitalWrite(kernePIN15, HIGH);  // Vpp must be STAY HIGH
+    digitalWrite(kernalPIN14, HIGH);  // Kernal: HIGH Address
+    digitalWrite(kernalPIN15, HIGH);  // Vpp must be STAY HIGH
   }
 
   EEPROM.put(10, state);
@@ -107,7 +115,18 @@ boolean isRestoreLongPressed() {
   return false;
 }
 
-
+void execReset() {
+  pinMode(resetPIN, OUTPUT);
+  digitalWrite(resetPIN, LOW);
+  // due to its design (single R for three LED's), colors can't be mixed in regular basis
+  // mixing WHITE by multiplexing RGB
+  for (int i = 0; i < 16; i++) {
+     red(); delay(10); green(); delay(10); blue(); delay(10);
+  } 
+  digitalWrite(resetPIN, HIGH);
+  pinMode(resetPIN, INPUT);
+  doState();
+}
 
 void setup() {
 
@@ -124,8 +143,8 @@ void setup() {
   analogWrite(ledB, 0);
 
 
-  pinMode(kernePIN14, OUTPUT);
-  pinMode(kernePIN15, OUTPUT);
+  pinMode(kernalPIN14, OUTPUT);
+  pinMode(kernalPIN15, OUTPUT);
   pinMode(restoreKey, INPUT);
   pinMode(resetPIN, INPUT);
 
@@ -134,6 +153,8 @@ void setup() {
   if(state < 0 || state > 3) state = 0;
  
   doState();
+
+  execReset();     // Power On reset
 }
 
 
@@ -155,20 +176,18 @@ void loop() {
   }
   else {
 
+    if (millis() - restoreHoldTime > 800) {    // after short press
+      doReset = true;
+    }
     restoreHoldTime = millis();
 
-    // * After Released Restore Key and State changed... RESET System *
+    // after releasing Restore key and possibly changing State, clear the flag and execute system RESET 
     if (doReset) {
       doReset = false;
 
-      pinMode(resetPIN, OUTPUT);
-      digitalWrite(resetPIN, LOW);
-      delay(500);
-      digitalWrite(resetPIN, HIGH);
-      pinMode(resetPIN, INPUT);
+      execReset();
     }
   }
-
 
   delay(10); // 10ms
 }
